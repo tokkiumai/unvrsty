fa86452ee82b572a5fbd2af09d56791970eeb97f
tokkiumai <kiwidancebad@gmail.com>
2024-07-17 11:43:11 +0300

    n.lab2

--- python/labs/l2.py
+++ python/labs/l2.py
@@ -57,3 +57,87 @@ def gf2_mul(a: str, b: str) -> str:
 
 
 def gf2_div(a: str, b: str) -> (str, str):
+    mb = []
+    rest = ""
+    r = a
+    while True:
+        m, r = gf2_core(r, b)
+        mb.append(m)
+        if m == "1" or m == "0":
+            rest = r
+            break
+        if r == "0":
+            break
+    multiplier = mb[0]
+    if len(mb) > 1:
+        for i in range(1, len(mb)):
+            multiplier = gf2_add(multiplier, mb[i])
+    return multiplier, rest
+
+
+def gf2_gcd(a: int, b: int) -> int:
+    a1 = a
+    b1 = b
+    result = math.nan
+    while True:
+        _, r = gf2_core(a1, b1)
+        if r == "0":
+            break
+        result = r
+        a1 = b1
+        b1 = r
+    return result
+
+
+def is_reducible(p: str) -> bool:
+    d = int("0b" + p, 2)
+    for i in range(2, d):
+        if gf2_div(p, bin(i)[2:])[1] == "0":
+            return False
+    return True
+
+
+def is_primitive(p: str) -> bool:
+    pow = len(p) - 1
+    m = 2 ** pow - 1
+    l = generate_polynomial(pow, m)
+    if gf2_div(l[len(l) - 1], p)[1] == "0":
+        for i in range(0, len(l) - 1):
+            d, m = gf2_div(l[i], p)
+            if m == "0":
+                return False
+        return True
+    return False
+
+
+def generate_polynomial(n: int, m: int) -> list[str]:
+    l = list()
+    for p in range(n, m):
+        new = "1" + "".join(["0" for _ in range(p)]) + "1"
+        l.append(new)
+    return l
+
+
+def get_primitive_roots(p: list[str], field: int) -> list[str]:
+    d = int("0b" + p, 2)
+    first_root = None
+    mod_l = None
+    for i in range(2, d):
+        x = i
+        mod_l = [bin(x)[2:]]
+        for j in range(2, field):
+            x *= i
+            mod = gf2_div(bin(x)[2:], p)[1]
+            mod_l.append(mod)
+            if mod == "1":
+                if j != field - 1:
+                    break
+                else:
+                    first_root = i
+        if first_root:
+            break
+    result = list()
+    for j in range(0, field - 1):
+        if gf2_gcd(j + 1, field - 1) == 1:
+            result.append(mod_l[j])
+    return result
