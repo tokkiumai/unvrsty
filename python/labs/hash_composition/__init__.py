ad3d37915c7ed2c2420282265960fa69a4c17a5b
tokkiumai <kiwidancebad@gmail.com>
2024-08-18 01:36:50 +0300

    n.diploma/str module

---
+++ python/labs/hash_composition/__init__.py
@@ -0,0 +1,5 @@
+from .rot_13 import hash_rot_13
+from .caesar import hash_caesar_cipher
+from .str_by_four import hash_str_by_four
+from .str_in_half import hash_str_in_half
+from .string import hash_str
